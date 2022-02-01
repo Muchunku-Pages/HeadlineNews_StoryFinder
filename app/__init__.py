ac93899fc056725a32182586b82c1a0e738d868d
Brian Muchunku <brauxer@gmail.com>
2022-02-01 22:42:44 +0300

    Formulate code in the main reference route 'home' application instance -errors.py file to catch errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from config import config_options
 
-#Initialization Assignment
-app = Flask(__name__)
 
-from app .home import views
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Generate the app configurations
+    app.config.from_object(config_options[config_name])
+
+   
+# Register Blueprint
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
+
+# Set config
+    from .request import configure_request
+    configure_request(app)
+
+ # To add views and forms
+
+    return app
+       
