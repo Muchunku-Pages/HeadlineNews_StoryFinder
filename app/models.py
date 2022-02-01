33f1375c42391bfbe1b639c4e1b28fe1dcfc8be4
Brian Muchunku <brauxer@gmail.com>
2022-02-01 09:07:07 +0300

    Add two additional classes ie. the class Category and the class Source to the existing Headline and Article Classes to better distinguish and implement the individual model roles within our Application

--- app/models.py
+++ app/models.py
@@ -1,14 +1,46 @@
-class HeadlineCategory:
+class Headline:
+    '''
+    Define the class from which the objects of the headline category of a news source are instantiated
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
 
 
+class Article:
+    '''
+    Define the class from which the objects of the news article of a news source are instantiated
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title  
 
+class Category:
+    '''
+    Define the class from which the objects of the news category of a news source are instantiated
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
 
-
-
-
-
-
-
-
-class HeadlineArticle:
-  
+class Source:
+    '''
+    Define the class from which the objects of news Source are instantiated
+    '''
+    def __init__(self,id,name,description,url):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
