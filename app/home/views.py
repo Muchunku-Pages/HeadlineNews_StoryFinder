e74cc9f55b4a487d5f221a67c1f64ea0337daa16
Brian Muchunku <brauxer@gmail.com>
2022-02-01 12:32:54 +0300

    Specify our Headline Finder application's basic path structure in the views.py template routing configuration file

--- app/home/views.py
+++ app/home/views.py
@@ -1,10 +1,32 @@
 from flask import render_template
-from app import app
+from . import home
 
-@app.route('/')
-def index():
+
+@home.route('/')
+def (home):
   '''
-  The view root page function that renders the index page and its data contents
+  Returns the home page landing page and its data content
   '''
-  message ='Howdy World'
-  return render_template('index.html', message = message)
+ 
+  return render_template('home.html')
+
+@home.route('/article')
+def article():
+
+    '''
+    Returns the article page containing the various article details and data
+    '''
+   
+   
+    return render_template('article.html')
+
+@home.route('/category/')
+def category(category_name):
+    '''
+     Returns the category.html page and its content
+    '''
+    category = get_category(category_name)
+    title = get_category(f'{category_name}')
+    category_name = category
+
+    return render_template('categories.html')
