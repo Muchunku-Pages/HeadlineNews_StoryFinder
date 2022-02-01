dce3d8fe366883435737404fb2b6ce7c87af335f
Brian Muchunku <brauxer@gmail.com>
2022-02-02 02:08:57 +0300

    Debug the 'home' decorator's home route method in views.py and complete assigning the routing functions from the requests.py in the view.py routing configuration code

--- app/home/views.py
+++ app/home/views.py
@@ -1,30 +1,41 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from . import home
+from ..request import get_source,article_source,get_category,get_headlinefrom . import home
 
 
 @home.route('/')
-def (home):
+def home():
+
+
   '''
   Returns the home page landing page and its data content
   '''
  
-  return render_template('home.html')
+  source= get_source()
+  headline = get_headline()
+  return render_template('home.html',source=source, headline = headline)
 
 @home.route('/article')
 def article():
-
-    '''
-    Returns the article page containing the various article details and data
-    '''
+  
+  '''
+  Returns the article page containing the various article details and data
+  '''
    
    
-    return render_template('article.html')
+  article = article_source(id)
+  return render_template('article.html',article= article,id=id )
 
-@home.route('/category/')
+@home.route('/category/<category_name>')
 def category(category_name):
+  
     '''
-     Return the category.html page and its content
+    Return the category.html page and its content
     '''
    
 
-    return render_template('categories.html')
+    category = get_category(category_name)
+    title = get_category(f'{category_name}')
+    category_name = category
+
+    return render_template('categories.html',title = title, category = category, category_name= category_name)
