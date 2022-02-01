e3cd3cb9d876d439e53397cfaa202d0be15c5ecd
Brian Muchunku <brauxer@gmail.com>
2022-02-01 21:08:21 +0300

    Generate another config.py globally and transfer all the configuration code with the exception of the key

--- instance/config.py
+++ instance/config.py
@@ -1,13 +1,15 @@
+#NEWS_API_KEY = 'd5cb1c4606844f8b91883d48a41ffbe0'
+
 import os
 
 class Config:
     '''
-    Configuration Parent class
-    Args:
+     Configuration Parent class
+      Args:
         Config: The parent configuration class with General configuration settings
     '''
     NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
-    CAT_API_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
+    # CAT_API_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
     CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
 
@@ -17,6 +19,7 @@ class Config:
 class ProdConfig(Config):
     '''
     Configuration Production Child class
+
     Args:
         Config: A child configuration class with Production configuration settings
     '''
